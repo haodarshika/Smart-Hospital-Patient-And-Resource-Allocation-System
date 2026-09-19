afb9744f99b120dfaf22068feb8a2208df1acb80
haodarshika <darshikaoshani@gmail.com>
2026-09-19 11:16:37 +0530

    Define max records and parallel array storage structures

--- main.c
+++ main.c
@@ -1,10 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#define maxRecords 100
 
 
 
+char patientNames[maxRecords][50];
+int patientAges[maxRecords];
+int triageLevel[maxRecords];
+int specialtyChoice[maxRecords];
+int isAdmitted[maxRecords];
+int assignedWard[maxRecords];
+int stayDays[maxRecords];
+
+
+int patientCount =0;
 int main(void){
     printf("-----Smart Hospital and Resource Allocation System-----\n");
     return 0;
-
 }
+
+
